b124c79aa773e935c79594424b798ba19b379c77
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-06-18 00:18:08 +0530

    async.js

--- day-6/async.js
+++ day-6/async.js
@@ -37,3 +37,50 @@ for (var i = 0; i <= 5; i++) {
 }
 
 
+
+
+
+// Promises part 2
+// async await function
+
+let step1 = function(){
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{
+            res("Select a photo");
+            rej("Erorr on photo")
+        },3000)
+    })
+}
+
+let step2 = function(){
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{
+            res("Filterrrrrr");
+            rej("Erorrr on filter")
+        },1000)
+    })
+}
+
+// to avoid call back hell condition, promises is created by developers 
+
+// step1().then((data1)=>{
+//     console.log(data1);
+//     return step2()
+// }).catch((erorr1)=>{
+//     console.log(erorr1); 
+// }).then((data2)=>{
+//     console.log(data2);  
+// }).catch((erorr2)=>{
+//     console.log(erorr2);  
+// })
+
+
+// async function
+async function call(){
+   let data1 = await step1()
+   console.log(data1);
+   
+   let data2 = await step2()
+   console.log(data2);  
+}
+call()
