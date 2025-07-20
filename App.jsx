09417f62f9dbd554b2d58cb56a8b0e9607819068
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-07-20 23:43:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,67 @@
-import React from 'react'
-import Day2 from './Day2'
+import { useState } from 'react'
+import Dummy from './Dummy'
+import Day4 from './Day4'
 
 
-const App = () => {
-  return (
-    <div>
-      <Day3/>
-    </div>
+// function App() {
+  // let a = 5;
+  // let b = 10;
+
+  // let marks = 80;
+  // function fun1() {
+  //   console.log(marks);
+  //   marks++;
+  //   console.log(marks);
+  // }
+
+  // const [marks,       setmarks] = useState(80);
+  // //    variable      function            value
+
+
+
+
+  // return(
+  //   <>                       
+  //    {/* this is empty tag to return all the things inside it 'Important' */}
+  //     {/* <h2>{a*b}</h2>
+  //     <div>{a+b}</div>
+  //     <h1>Hello</h1> */}
+
+  //     <h1>Marks is : {marks}</h1>
+  //     <button onClick={()=>{
+  //       // fun1();
+  //       setmarks(marks+1);
+  //     }}>Incriment</button>
+  //   </>
+
+
+
+  // return(
+  //   <>
+  //   <h2>Shashank</h2>
+  //   <div id='flex'>
+  //     <h4>About</h4>
+  //     <h4>Skills</h4>
+  //     <h4>Contact</h4>
+  //     <h4>Submit</h4>
+  //   </div>
+
+
+  //   </>
+
+
+  // class ka item
+    const App = () => {
+      let user = "helloooo"
+      return(
+        <div>
+          <Day4    data={user} />
+        </div>
+
+
+
+
+
   )
 }
-
 export default App
