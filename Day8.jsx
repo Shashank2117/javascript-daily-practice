a724db75d8674436f3124b32b5660e00991c5338
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-08-03 23:58:41 +0530

    React.js (Day8 )

---
+++ Day8.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Day8CSS.css'
+import Shashank from './Shashank.jsx'
+
+const Day8 = () => {
+  return (
+    <div>
+        <Shashank/>
+    </div>
+  )
+}
+
+export default Day8
