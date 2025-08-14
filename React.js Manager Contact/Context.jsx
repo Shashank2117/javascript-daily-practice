b8cafb47d4f541b7d9aa505c11f675144a1e6fef
Shashank kanojiya <kanojiyashashank2004@gmail.com>
2025-08-14 19:59:56 +0530

    Add files via upload

---
+++ React.js Manager Contact/Context.jsx
@@ -0,0 +1,5 @@
+import { createContext, useReducer, useEffect } from 'react'
+
+const Context= createContext()
+
+export default Context
---
+++ React.js Manager Contact/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+// import  ContextProvider from './Context.jsx'
+// import {BrowserRouter}  from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  
+  <StrictMode>
+    {/* <ContextProvider> 
+      <BrowserRouter>  */}
+        <App />
+      {/* </BrowserRouter>
+    </ContextProvider> */}
+  </StrictMode>,
+
+)
